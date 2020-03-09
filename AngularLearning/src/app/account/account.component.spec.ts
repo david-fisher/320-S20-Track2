339d53992a0c71e24b267f874afd482c5cb6ad26
Caleb Coffin <coffincw@gmail.com>
2020-03-09 16:19:39 -0400

    add navbar and routing

--- AngularLearning/src/app/account/account.component.spec.ts
+++ AngularLearning/src/app/account/account.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CourseComponent } from './course.component';
+import { AccountComponent } from './account.component';
 
-describe('CourseComponent', () => {
-  let component: CourseComponent;
-  let fixture: ComponentFixture<CourseComponent>;
+describe('AccountComponent', () => {
+  let component: AccountComponent;
+  let fixture: ComponentFixture<AccountComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CourseComponent ]
+      declarations: [ AccountComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CourseComponent);
+    fixture = TestBed.createComponent(AccountComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- AngularLearning/src/app/app.module.ts
+++ AngularLearning/src/app/app.module.ts
@@ -2,19 +2,48 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CourseComponent } from './course/course.component';
 import { TestComponent } from './test/test.component';
 import {FormsModule} from '@angular/forms';
+import { NavComponent } from './nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './account/account.component';
+import { FaqComponent } from './faq/faq.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: TestComponent },
+  { path: 'faq', component: FaqComponent},
+  { path: 'account', component: AccountComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseComponent,
-    TestComponent
+    TestComponent,
+    NavComponent,
+    AccountComponent,
+    FaqComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
