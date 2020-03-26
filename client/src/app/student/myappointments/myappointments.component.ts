0e3aa458dd3da66a97786dab2617f01a1703c297
Zach Schaffer <zachary.schaffer12@gmail.com>
2020-03-26 15:42:37 -0400

    Create example appointments

--- client/src/app/student/myappointments/myappointments.component.ts
+++ client/src/app/student/myappointments/myappointments.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {APPOINTMENTS} from './mock-appointments';
-
+import {Appointments} from './appointments';
 
 
 @Component({
@@ -10,48 +10,13 @@ import {APPOINTMENTS} from './mock-appointments';
 })
 export class MyappointmentsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
+
+  get appointments(): Array<Appointments> {
+    return APPOINTMENTS;
+  }
 
   ngOnInit(): void {
-    const today: Date = new Date();
-    const upcomingApp = document.getElementById('next');
-    const previousApps = document.getElementById('prev');
-    // tslint:disable-next-line:prefer-for-of
-    for (let x = 0; x < APPOINTMENTS.length; x++) {
-      console.log(APPOINTMENTS[x].date);
-      if (APPOINTMENTS[x].date > today) {
-        const newApp = document.createElement('div');
-        newApp.style.border = 'solid';
-        newApp.style.borderRadius = '5px';
-        newApp.style.width = '700px';
-        newApp.style.height = '50px';
-        newApp.style.marginBottom = '20px';
-        const text = document.createElement('h2');
-        let innertext = APPOINTMENTS[x].date.getHours() + ':' + APPOINTMENTS[x].date.getMinutes() + '    ';
-        innertext = innertext + APPOINTMENTS[x].date.getDate() + '//' + (APPOINTMENTS[x].date.getMonth() + 1) + '//20     ||  ';
-        innertext = innertext + APPOINTMENTS[x].type + '   ||   ' + APPOINTMENTS[x].supporter;
-        text.innerText = innertext;
-        // text.style.textAlign = 'center';
-        newApp.prepend(text);
-        upcomingApp.appendChild(newApp);
-        console.log('coming up');
-      } else {
-        const newApp = document.createElement('div');
-        newApp.style.border = 'solid';
-        newApp.style.borderRadius = '5px';
-        newApp.style.width = '700px';
-        newApp.style.height = '50px';
-        newApp.style.marginBottom = '20px';
-        const text = document.createElement('h2');
-        let innertext = APPOINTMENTS[x].date.getHours() + ':' + APPOINTMENTS[x].date.getMinutes() + '    ';
-        innertext = innertext + APPOINTMENTS[x].date.getDate() + '//' + (APPOINTMENTS[x].date.getMonth() + 1) + '//20     ||  ';
-        innertext = innertext + APPOINTMENTS[x].type + '   ||   ' + APPOINTMENTS[x].supporter;
-        text.innerText = innertext;
-        // text.style.textAlign = 'center';
-        newApp.prepend(text);
-        previousApps.appendChild(newApp);
-        console.log('past');
-      }
-    }
   }
 }
