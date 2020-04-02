32e47fb482c81586aa917d0632a9d12d6f525f67
ssd500 <42982407+ssd500@users.noreply.github.com>
2020-04-02 00:25:03 -0400

    Make handler call its helpers

--- fistbump-lambdas/appointments-get/handler.py
+++ fistbump-lambdas/appointments-get/handler.py
@@ -1,6 +1,8 @@
 import json
 
-  
+def parse_path(path):
+	return path.split('/')
+
 def get_appointments(email):
 	app = query_the_database1(email)
 	return app
@@ -10,9 +12,12 @@ def get_single_appointment(email, app_id):
 	return single_app
 
 def lambda_handler(event, context):
-    # TODO implement
-
+    path = parse_path(event['path'])
+	if len(path) == 3 and path[0] == 'supporters' and path[2] == 'appointments': #first helper call
+		body = {list: get_appointments(path[1])}
+	elif len(path) == 4 and path[0] == 'supporters' and path[2] == 'appointments': #second helper call
+		body = {list: get_single_appointment(path[1], path[3])}
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': body
     }
