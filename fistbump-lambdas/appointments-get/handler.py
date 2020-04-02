452c4d01f4f37b58092ed31b831eb56627b7daf6
ssd500 <42982407+ssd500@users.noreply.github.com>
2020-04-01 23:47:32 -0400

    Consolidate helper functions with handler

--- fistbump-lambdas/appointments-get/handler.py
+++ fistbump-lambdas/appointments-get/handler.py
@@ -1,5 +1,14 @@
 import json
 
+  
+def get_appointments(email):
+	app = query_the_database1(email)
+	return app
+
+def get_single_appointment(email, app_id):
+	single_app = query_the_database2(email, app_id)
+	return single_app
+
 def lambda_handler(event, context):
     # TODO implement
 
