21c712ab64858f1c728332d1cf90c4aa65fd8199
Caleb Coffin <coffincw@gmail.com>
2020-04-02 14:18:55 -0400

    added more shadow to cards and made the horizontal line smaller

--- client/src/app/student/student-myappointments/mock-appointments.ts
+++ client/src/app/student/student-myappointments/mock-appointments.ts
@@ -4,10 +4,12 @@ import {Appointments} from './appointments';
 const app1 = new Date(2020, 3, 23, 15, 20, 0, 0);
 const app2 = new Date(2020, 3, 17, 11, 45, 0, 0);
 const app3 = new Date(2020, 1, 11, 9, 10, 0, 0);
+const app4 = new Date(2020, 1, 15, 9, 10, 0, 0);
 
 
 export const APPOINTMENTS: Appointments[] = [
   {date: app1, type: 'Resume Help', supporter: 'Brian Smith'},
   {date: app2, type: 'Interview Help', supporter: 'John Doe'},
-  {date: app3, type: 'Career Guidance', supporter: 'John Doe'}
+  {date: app3, type: 'Career Guidance', supporter: 'John Doe'},
+  {date: app4, type: 'Job Search', supporter: 'David Fisher'}
 ];
