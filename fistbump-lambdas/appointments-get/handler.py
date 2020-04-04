68f32f314be965d53209ce26fb2c205c43f6f8e8
ssd500 <42982407+ssd500@users.noreply.github.com>
2020-04-04 14:53:08 -0400

    Change code to match query string mapped to event

--- fistbump-lambdas/appointments-get/handler.py
+++ fistbump-lambdas/appointments-get/handler.py
@@ -1,23 +1,31 @@
 import json
 
-def parse_path(path):
-	return path.split('/')
+# def parse_path(path):
+# 	return path.split('/')
 
 def get_appointments(email):
 	app = query_the_database1(email)
 	return app
 
-def get_single_appointment(email, app_id):
-	single_app = query_the_database2(email, app_id)
+def get_single_appointment(app_id):
+	single_app = query_the_database2(app_id)
 	return single_app
 
 def lambda_handler(event, context):
-    path = parse_path(event['path'])
-	if len(path) == 3 and path[0] == 'supporters' and path[2] == 'appointments': #first helper call
-		body = {list: get_appointments(path[1])}
-	elif len(path) == 4 and path[0] == 'supporters' and path[2] == 'appointments': #second helper call
-		body = {list: get_single_appointment(path[1], path[3])}
+#     path = parse_path(event['path'])
+# 	if len(path) == 3 and path[0] == 'supporters' and path[2] == 'appointments': #first helper call
+# 		body = {list: get_appointments(path[1])}
+# 	elif len(path) == 4 and path[0] == 'supporters' and path[2] == 'appointments': #second helper call
+# 		body = {list: get_single_appointment(path[1], path[3])}
+	statusCode = 200
+	body = {}
+	if event.user_id:
+		body = {list: get_appointments(event.user_id)}
+	elif event.appointment_id:
+		body = {list: get_single_appointment(event.appointment_id)}
+	else:
+		statusCode = 404
     return {
-        'statusCode': 200,
+        'statusCode': statusCode,
         'body': body
     }
