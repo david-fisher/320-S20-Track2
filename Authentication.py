7ddf29d998ad07cebd6bd26389093b3cd4da8f49
ssd500 <42982407+ssd500@users.noreply.github.com>
2020-04-11 19:31:00 -0400

    Add error handling

--- Authentication.py
+++ Authentication.py
@@ -7,14 +7,41 @@ def authentication_login(event, context):
 
     #Connect to Database
     client = boto3.client('rds-data')
-
+    user = client.execute_statement(
+    secretARN = "",
+    database = "",
+    resourceARN = "",
+    sql = "SELECT email FROM user WHERE email = '%s';" % (username)
+    )
+    
+    statusCode = 200
     loginResponse = {}
-    loginResponse['username'] = username
-    loginResponse['password'] = password
-    loginResponse['message'] = 'Login Sucess'
+    
+    #Check if user exists
+    if(user['records'] == []):
+        loginResponse['message'] = 'User DNE'
+        statusCode = 404
+    
+    #Check if password is correct
+    client = boto3.client('rds-data')
+    pw = client.execute_statement(
+    secretARN = "",
+    database = "",
+    resourceARN = "",
+    sql = "SELECT password FROM user WHERE email = '%s';" % (username)
+    )
+    
+    if(not verify_password(password, pw['records'][0][0]['stringValue'])):
+        loginResponse['message'] = 'Password Does Not Match'
+        statusCode = 404
+    
+    if(statusCode == 200):
+        loginResponse['username'] = username
+        loginResponse['password'] = password
+        loginResponse['message'] = 'Login Success'
 
     response = {}
-    response['statesCode'] = 200
+    response['statusCode'] = statusCode
     response['header'] = {}
     response['header']['Content-Type'] = 'application/json'
     response['body'] = json.dumps(loginResponse)
