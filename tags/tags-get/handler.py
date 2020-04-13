b756000ebf1fddf708992a48b2ccac9c46165d18
Jake Goldman <jacob.lh.goldman@gmail.com>
2020-04-13 18:29:46 -0400

    Completed the tags-get lambda (untested)

--- tags/tags-get/handler.py
+++ tags/tags-get/handler.py
@@ -1,8 +1,36 @@
 import json
+import pymysql
+
+
+#rds settings
+rds_host  = "rds-instance-endpoint"
+name = 'rds_config.db_username'
+password = 'rds_config.db_password'
+db_name = 'rds_config.db_name'
+
+
+def get_tags():
+    # should be error-checked
+    conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+
+    try:
+        with conn.cursor() as curr:
+            sql = "SELECT * FROM tags;"
+            conn.execute(sql)
+            response = curr.fetchall()
+            print(response)
+    finally:
+        conn.close()
+    return response
+
+
+
+
+
 
 def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(get_tags())
     }
