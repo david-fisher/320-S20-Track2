75bdd713dac49c56fd1a24adb2115b19ade1a570
Jake Goldman <jacob.lh.goldman@gmail.com>
2020-04-13 20:20:09 -0400

    Wrote preliminary tags-put

--- tags/tags-put/handler.py
+++ tags/tags-put/handler.py
@@ -1,8 +1,48 @@
 import json
+import pymysql
+import rds_config
+
+
+#rds settings
+rds_host  = rds_config.endpoint
+name = rds_config.username
+password = rds_config.password
+db_name = rds_config.db_name
+
+
+def put_tag(tag_name):
+    # should be error-checked
+    conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+
+    with conn.cursor() as curr:
+        #generate the next id number
+        sql = "SELECT MAX(tag_id) FROM tags;"
+        conn.execute(sql)
+        response = curr.fetchone()
+        tag_id = response + 1
+
+        sql = "INSERT INTO tags VALUES (%s, %s);"
+        conn.execute(sql, [tag_id, tag_name])
+    conn.close()
+    return 200
+
+
+
+
+
 
 def lambda_handler(event, context):
-    # TODO implement
+
+    # extract the tag_name
+    tag_name = event["body"]["tag_name"]
+    tag_id = int(event["body"]["tag_name"])
+
+    response = put_tag(tag_name)
+
+    statusCode = response
+    message = "tag {} successfully dropped".format(tag_id)
+
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'statusCode': statusCode,
+        'body': {'message' : message}
     }
