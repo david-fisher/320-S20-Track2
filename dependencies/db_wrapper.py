e7f012d6afeaf0c5fb65fd14a772cee6e4d37545
Jake Goldman <jacob.lh.goldman@gmail.com>
2020-04-16 19:20:50 -0400

    updated in-code documentation and standardized return format

--- dependencies/db_wrapper.py
+++ dependencies/db_wrapper.py
@@ -1,7 +1,17 @@
 import boto3
 import rds_config
 
-def execute_statement(client, sql, sql_parameters=[]):
+'''
+    args:
+        sql: the SQL statement to be executed
+        sql_parameters: the properly formated sql_parameters to be applied
+
+    returns:
+        The response from the database in boto3 format
+'''
+def execute_statement(sql, sql_parameters=[]):
+    client = boto3.client('rds-data')
+
     response = client.execute_statement(
         secretArn=rds_config.SECRET_ARN,
         database=rds_config.DB_NAME,
@@ -11,6 +21,15 @@ def execute_statement(client, sql, sql_parameters=[]):
     )
     return response
 
+'''
+    args:
+        query_result: the return value of a boto3 execute_statement call
+
+    returns:
+        A list representing the retrieved records from a call to execute_statement.
+        Each element in the list is a tuple with the retrieved fields as values
+        in the same order as they would be in the database.
+'''
 def extract_records(query_result):
     records = []
     for record in query_result['records']:
--- tags/tags-get/handler.py
+++ tags/tags-get/handler.py
@@ -5,11 +5,10 @@ from db_wrapper import execute_statement, extract_records
 
 
 def get_tags():
-    client = boto3.client('rds-data')
 
     # query the database for all current tags
     sql = "SELECT * FROM tags;"
-    query_result = execute_statement(client, sql)
+    query_result = execute_statement(sql)
 
     # parse the result
     response = extract_records(query_result)
@@ -17,11 +16,14 @@ def get_tags():
 
 
 def lambda_handler(event, context):
-    response = get_tags()
+    response_body = json.dumps(get_tags())
+    # There's no input, so statusCode should be 200 unleses get_tags() throws an error
+    response_status = 200
 
-    statusCode = 200
-
-    return {
-        'statusCode': statusCode,
-        'body': json.dumps(response)
+    response = {
+        'statusCode' : response_status,
+        'body' : response_body
     }
+    print(response)
+
+    return response
