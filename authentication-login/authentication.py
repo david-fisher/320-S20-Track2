45d56649e649988a9314f1af6fb4aa112c2d4f4d
ssd500 <42982407+ssd500@users.noreply.github.com>
2020-04-17 00:59:20 -0400

    Update event to match proxy integ & param sql

--- authentication-login/authentication.py
+++ authentication-login/authentication.py
@@ -2,6 +2,8 @@ import json
 import random
 import string
 import boto3
+import rds_config
+from db_wrapper import execute_statement, extract_records
 
 def token(N=64):
     return ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase, k=N))
@@ -12,29 +14,36 @@ def authentication_login(event, context):
 
     #Connect to Database
     client = boto3.client('rds-data')
-    user = client.execute_statement(
-    secretARN = "",
-    database = "",
-    resourceARN = "",
-    sql = "SELECT email FROM user WHERE email = '%s';" % (username)   #Convert to db_wrapper
-    )
+    
+    sql = "SELECT email FROM `user` WHERE `email` = :username;"
+    sql_parameters = [ {'name':'username', 'value':{'stringValue': '{username}'} ]
+    user = execute_statement(client, sql, sql_parameters)
+#     user = client.execute_statement(
+#     secretARN = "",
+#     database = "",
+#     resourceARN = "",
+#     sql = "SELECT email FROM user WHERE email = '%s';" % (username)   #Convert to db_wrapper
+#     )
     
     statusCode = 200
     loginResponse = {}
     
     #Check if user exists
-    if(user['records'] == []):
+    if(extract_result(user) == []):
         loginResponse['message'] = 'User DNE'
         statusCode = 404
     
     #Check if password is correct
-    client = boto3.client('rds-data')
-    pw = client.execute_statement(
-    secretARN = "",
-    database = "",
-    resourceARN = "",
-    sql = "SELECT password FROM user WHERE email = '%s';" % (username)
-    )
+    sql = "SELECT password FROM user WHERE email = :username;"
+    sql_parameters = [ {'name':'username', 'value':{'stringValue': '{username}'} ]
+    pw = execute_statement(client, sql, sql_parameters)
+#     client = boto3.client('rds-data')
+#     pw = client.execute_statement(
+#     secretARN = "",
+#     database = "",
+#     resourceARN = "",
+#     sql = "SELECT password FROM user WHERE email = '%s';" % (username)
+#     )
     
     if(not verify_password(password, pw['records'][0][0]['stringValue'])):
         loginResponse['message'] = 'Password Does Not Match'
