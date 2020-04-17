1db8e7ce53ab6c4e8baf19984b6c1fe1038bd62b
Kathleen <kathleen@Kathleens-MacBook-Pro.local>
2020-04-16 20:27:14 -0400

    init appointments-patch

---
+++ appointments/appointments-patch.py
@@ -0,0 +1,39 @@
+import json
+import boto3
+import rds_config
+from db_wrapper import execute_statement, extract_records
+
+# Cancels the appt by setting the 'cancelled' boolean in the db to true
+# Input: appt_id
+# Output: 200 for success or 404 if appt id not found in db
+def cancel_appt(event, context):
+     # extract the appt id to delete
+    appt_id = event['appointment_id']
+
+    # Connect to database
+    client = boto3.client('rds-data')
+
+    # Check if the appt id exists in database
+    query = "SELECT `appt_id` FROM `appointments` WHERE `appointment_id` = :appt_id;"
+    sql_params = [{'name': 'appt_id', 'value':{'longValue': appt_id}}]
+    existing_appt = execute_statement(client, query, sql_params)
+    
+    # Appt id not found
+    if existing_appt['records'] == []:
+        return {
+            'status code': 404,
+            'body': json.dumps('Appointment id not found in database')
+        }
+
+    # Cancel the appt
+    query = "UPDATE `appointments` SET `cancelled` = true WHERE appointment_id = :appt_id;"
+    query_response = execute_statement(client, query, sql_params)
+
+    # Return success 
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Successfully cancelled appointment')
+    }
+
+
+    
