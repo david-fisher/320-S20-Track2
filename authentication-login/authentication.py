e08e5d93ff523a591095a65737c6cd0bb1a5edf6
ssd500 <42982407+ssd500@users.noreply.github.com>
2020-04-17 00:03:38 -0400

    Adding lambda to branch

---
+++ authentication-login/authentication.py
@@ -0,0 +1,55 @@
+import json
+import random
+import string
+import boto3
+
+def token(N=64):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase, k=N))
+
+def authentication_login(event, context):
+    username = event['body']['username']
+    password = event['body']['password']
+
+    #Connect to Database
+    client = boto3.client('rds-data')
+    user = client.execute_statement(
+    secretARN = "",
+    database = "",
+    resourceARN = "",
+    sql = "SELECT email FROM user WHERE email = '%s';" % (username)   #Convert to db_wrapper
+    )
+    
+    statusCode = 200
+    loginResponse = {}
+    
+    #Check if user exists
+    if(user['records'] == []):
+        loginResponse['message'] = 'User DNE'
+        statusCode = 404
+    
+    #Check if password is correct
+    client = boto3.client('rds-data')
+    pw = client.execute_statement(
+    secretARN = "",
+    database = "",
+    resourceARN = "",
+    sql = "SELECT password FROM user WHERE email = '%s';" % (username)
+    )
+    
+    if(not verify_password(password, pw['records'][0][0]['stringValue'])):
+        loginResponse['message'] = 'Password Does Not Match'
+        statusCode = 404
+    
+    if(statusCode == 200):
+        loginResponse['username'] = username
+        loginResponse['password'] = password
+        loginResponse['message'] = 'Login Success'
+        loginResponse['token'] = token
+
+    response = {}
+    response['statusCode'] = statusCode
+    response['header'] = {}
+    response['header']['Content-Type'] = 'application/json'
+    response['body'] = json.dumps(loginResponse)
+
+    return response
