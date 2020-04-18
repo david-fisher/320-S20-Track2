084ad6195c4e3ad8c18c91606e7335cbd4035e46
Caleb Coffin <coffincw@gmail.com>
2020-04-17 21:18:02 -0400

    add boostrap in component

--- client/src/app/faq/faq.component.ts
+++ client/src/app/faq/faq.component.ts
@@ -7,13 +7,14 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./faq.component.css']
 })
 export class FaqComponent implements OnInit {
-
+  public test1;
   constructor(private http: HttpClient) {
   } // You can leave the constructor blank, http will be automatically "injected" for you
 
   test() { // Function which you can call to make a request
-    this.http.get('http://t2-bucket-storage.s3-website.us-east-2.amazonaws.com/faq').subscribe(res => {
+    this.http.get('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/faq').subscribe(res => {
       console.log(res);
+      this.test1 = res;
     });
   }
 
