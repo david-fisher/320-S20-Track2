228c894de0ee50a74cef18f4ca00c734f4c70e97
Kathleen <kathleengiang7@gmail.com>
2020-04-17 22:27:25 -0400

    Add error checking, update imports

--- appointments/appointments-patch.py
+++ appointments/appointments-patch.py
@@ -1,6 +1,4 @@
 import json
-import boto3
-import rds_config
 from db_wrapper import execute_statement, extract_records
 
 # Cancels the appt by setting the 'cancelled' boolean in the db to true
@@ -8,13 +6,7 @@ from db_wrapper import execute_statement, extract_records
 # Output: 200 for success or 404 if appt id not found in db
 def cancel_appt(event, context):
     # extract the appt id to delete
-    if event == {}:
-        return {
-            'statusCode': 400,
-            'body': 'event is empty {}, path parameter {id} was not properly extracted into event'
-        }
-    else:
-        appt_id = int(event['pathParameters']['id'])
+    appt_id = int(event['pathParameters']['id'])
 
     # Check if the appt id exists in database
     query = "SELECT `appointment_id` FROM `appointments` WHERE `appointment_id` = :appt_id;"
@@ -22,7 +14,7 @@ def cancel_appt(event, context):
     existing_appt = execute_statement(query, sql_params)
     
     # Appt id not found
-    if existing_appt['records'] == []:
+    if existing_appt == []:
         return {
             'status code': 404,
             'body': json.dumps('Appointment id not found in database')
@@ -31,6 +23,13 @@ def cancel_appt(event, context):
     # Cancel the appt
     query = "UPDATE `appointments` SET `cancelled` = true WHERE `appointment_id` = :appt_id;"
     query_response = execute_statement(query, sql_params)
+
+    # If the database does not update the appt to cancelled
+    if query_response['numberOfRecordsUpdated'] == 0:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('Error in cancelling the appointment')
+        }
 
     # Return success 
     return {
