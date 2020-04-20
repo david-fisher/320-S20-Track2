a6982903746e69fc73288eeb21d027749e61ebb0
Caleb Coffin <coffincw@gmail.com>
2020-04-20 14:05:05 -0400

    Add create supporter card and start setting up post request

--- client/src/app/createaccount/createaccount.component.ts
+++ client/src/app/createaccount/createaccount.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-createaccount',
@@ -9,11 +10,22 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class CreateaccountComponent implements OnInit {
   userType;
+  typeOfUser;
+  userName: string;
+  preferredName: string;
+  gpa: string;
+  gradYear: string;
+  github: string;
+  linkedin: string;
+  supporterType: Array<string>;
+  userTitle: string;
+  currentEmployer: string;
   form: FormGroup;
   public myGroup;
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) {
     this.userType = this.activatedRoute.snapshot.params.type === 'student';
+    this.typeOfUser = this.activatedRoute.snapshot.params.type;
     this.myGroup = new FormGroup({
       firstName: new FormControl()
     });
@@ -22,10 +34,18 @@ export class CreateaccountComponent implements OnInit {
   ngOnInit(): void {
   }
   sendData() {
-    if (this.userType) {
-
-    } else {
-
-    }
+    const data = {
+      'userType': this.typeOfUser,
+      'userName': this.userName,
+      'preferredName': this.preferredName,
+      'gpa': this.gpa,
+      'gradYear': this.gradYear,
+      'github': this.github,
+      'linkedin': this.linkedin,
+      'supporterType': this.supporterType,
+      'userTitle': this.userTitle,
+      'currentEmployer': this.currentEmployer
+    };
+    this.http.post<JSON>("https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/account", data)
   }
 }
