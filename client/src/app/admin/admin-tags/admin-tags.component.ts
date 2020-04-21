89724016548096c02d7233d39b98fa1e461698fd
JonathanSigel2000 <jsigel@umass.edu>
2020-04-21 19:07:13 -0400

    Getting a putting tags now works

--- client/src/app/admin/admin-tags/admin-tags.component.ts
+++ client/src/app/admin/admin-tags/admin-tags.component.ts
@@ -3,6 +3,7 @@ import {InterestTags} from './interest-tag';
 import {TAGS} from './tag-list';
 import {HttpClient, HttpResponse} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {stringify} from "querystring";
 
 @Component({
   selector: 'app-admin-tags',
@@ -12,37 +13,36 @@ import {Observable} from 'rxjs';
 export class AdminTagsComponent implements OnInit {
   tagInput;
   selectedTags;
+  pageTags;
 
   constructor(private http: HttpClient) {
+    this.pageTags = this.tags_https;
   }
 
-  get tag_list(): Array<InterestTags> {
-    return TAGS;
-  }
-
-  get tags_https(): Array<InterestTags> { // Function which you can call to make a request
-    const input = this.http.get('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/tags')
-    const response = Object.keys(input);
-    let result = [];
-    for (const tag of response) {
-      result.push(tag);
-    }
+  get tags_https(): Array<InterestTags> {
+    const result = [];
+    this.http.get('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/tags', {}).subscribe(res => {
+      for (const tag of res) {
+        const newTag = {name: tag[1]}
+        result.push(newTag);
+      }
+    });
+    console.log(result);
     return result;
   }
 
-  push_tag(): Array<InterestTags> {
+  push_tag() {
     if (this.tagInput.length > 0) {
-      for (const entry of TAGS) {
+      for (const entry of this.pageTags) {
         if (entry.name === this.tagInput) {
           alert('This tag already exists, please input another..');
-          return TAGS;
         }
       }
       if (confirm('Is this the tag you want? Tag: ' + this.tagInput)) {
-        TAGS.push({name: this.tagInput});
+        this.http.put('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/tags', {tag_name: this.tagInput}).subscribe();
       }
     }
-    return TAGS;
+    this.pageTags = this.tags_https;
   }
 
   delete_tag(): Array<InterestTags> {
