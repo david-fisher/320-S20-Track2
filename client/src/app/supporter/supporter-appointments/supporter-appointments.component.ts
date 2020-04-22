ca1cccba9cc20ba0affd0c82122ec31a62fdd7f0
Bruce Waters <57501469+watersbruce@users.noreply.github.com>
2020-04-22 15:36:25 -0400

    get appointments starter code

--- client/src/app/supporter/supporter-appointments/supporter-appointments.component.ts
+++ client/src/app/supporter/supporter-appointments/supporter-appointments.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {APPOINTMENTS} from './appointments';
 import {Appointments} from './appointments';
 import { Router } from '@angular/router';
+import {InterestTags} from "../../admin/admin-tags/interest-tag";
+import {HttpClient, HttpResponse} from '@angular/common/http';
+
 @Component({
   selector: 'app-supporter-appointments',
   templateUrl: './supporter-appointments.component.html',
@@ -9,19 +12,37 @@ import { Router } from '@angular/router';
 })
 export class SupporterAppointmentsComponent implements OnInit {
 
-  constructor() { }
+  pageAppointments;
 
-  get appointments(): Array<Appointments> {
-    return APPOINTMENTS;
+  constructor(private http: HttpClient) {
+    this.pageAppointments = this.appointments;
   }
+
+  // get appointments(): Array<Appointments> {
+  //   return APPOINTMENTS;
+  // }
 
   get date(): object {
     return new Date();
   }
 
+  get appointments(): Array<Appointments> {
+    const result = [];
+    this.http.get('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/appointments/1', {}).subscribe(res => {
+      console.log(res)
+      // @ts-ignore
+      for (const appt of res) {
+        const newAppt = {name: appt[1]}
+        result.push(newAppt);
+      }
+    });
+    console.log(result);
+    return result;
+  }
+
   verify(appointment) {
     if (confirm('Are you sure you want to cancel?')) {
-      prompt('Please state a reason for cancelation.');
+      prompt('Please state a reason for cancellation.');
       for (const x in APPOINTMENTS) {
         if (APPOINTMENTS[x] === appointment) {
           console.log(parseInt(x));
