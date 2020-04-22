220a05d4a9add36a041a54d8284705c56f691cdd
NishantAcharya <36997477+NishantAcharya@users.noreply.github.com>
2020-04-22 17:12:33 -0400

    Added format checks for date/time string
    Added format checks for date and time strings which send status 400 if the regex is not matched

--- appointment/lambda_function.py
+++ appointment/lambda_function.py
@@ -63,7 +63,22 @@ def lambda_handler(event, context):
         }
     
     #Checking if the given date string is in the correct format or not
+    date_re = re.compile('^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$')
+    #Returning bad request if the format is wrong
+    if not(date_re.match(appt_date)):
+        return{
+           'body': json.dumps("Date bad format!"),
+            'statusCode': 400
+        }
     
+    #Checking if the given time string is in the correct format or not
+    time_re = re.compile('^([0-1][0-9]|[2][0-3]):([0-5][0-9]):([0-5][0-9])$')
+    #Returning bad request if the format is wrong
+    if not(time_re.match(start_time)):
+        return{
+           'body': json.dumps("Time bad format!"),
+            'statusCode': 400
+        }
     
     # Generating a new appointment_id for the current appointment by adding 1 to the last id
     query = "SELECT appointment_id FROM appointments ORDER BY appointment_id DESC LIMIT 1"
