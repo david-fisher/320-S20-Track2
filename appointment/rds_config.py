f685972474d09c022a6b7e187d194d239c25ee61
NishantAcharya <36997477+NishantAcharya@users.noreply.github.com>
2020-04-22 04:28:24 -0400

    Inital Lambda made
    Initial lambda made
    requires changes to date time

---
+++ appointment/rds_config.py
@@ -0,0 +1,10 @@
+# for boto3
+ARN = 'arn:aws:rds:us-east-2:500514381816:cluster:t2-database'
+SECRET_ARN = 'arn:aws:secretsmanager:us-east-2:500514381816:secret:rds-db-credentials/cluster-B7EKM7ILLEBW4QPTQ2NA3RCZUU/T2_Cloud-VO3HhS'
+DB_NAME = 'test'
+
+# for pymysql
+endpoint  = "t2-database.cluster-cxxkndc4kgoa.us-east-2.rds.amazonaws.com"
+username  = 'T2_Cloud'
+password  = 'zbmml8qlaQmpRvZSMb2F'
+db_name   = 'test'
