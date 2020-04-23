c66a271ec5b6d76158fb694036de6ff0ffb0c82c
Bruce Waters <57501469+watersbruce@users.noreply.github.com>
2020-04-22 22:03:33 -0400

    supporter appointments

--- client/src/app/supporter/supporter-appointments/supporter-appointments.component.ts
+++ client/src/app/supporter/supporter-appointments/supporter-appointments.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {TEST_APPOINTMENTS} from './appointments';
-import {Appointments} from './appointments';
+import {Appointment} from './appointments';
 import { Router } from '@angular/router';
 import {InterestTags} from "../../admin/admin-tags/interest-tag";
 import {HttpClient, HttpResponse} from '@angular/common/http';
@@ -19,27 +19,27 @@ export class SupporterAppointmentsComponent implements OnInit {
     this.pageAppointments = this.appointments;
   }
 
-  get appointments(): Array<Appointments> {
-    return TEST_APPOINTMENTS;
-  }
+  // get appointments(): Array<Appointments> {
+  //   return TEST_APPOINTMENTS;
+  // }
 
   get date(): object {
     return new Date();
   }
 
-  // get appointments(): Array<Appointments> {
-  //   const result = [];
-  //   this.http.get('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/appointments/1', {}).subscribe(res => {
-  //     console.log(res)
-  //     // @ts-ignore
-  //     for (const appt of res) {
-  //       const newAppt = {name: appt[1]}
-  //       result.push(newAppt);
-  //     }
-  //   });
-  //   console.log(result);
-  //   return result;
-  // }
+  get appointments(): Array<Appointment> {
+    const result = [];
+    this.http.get('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/appointments/1', {}).subscribe(res => {
+      console.log(Object.values(res));
+      for (const appt of Object.values(res)) {
+        //const newAppt = {name: appt[1]};
+        const newAppt : Appointment = {date: new Date(appt[2].split("-")[0], appt[2].split("-")[1], appt[2].split("-")[2], appt[3].split(":")[0], appt[3].split(":")[1], appt[3].split(":")[2], 0), type: "Meeting Type: "+appt[6], student: 'User-ID: '+appt[0], location: "Meeting Location", duration: appt[4]};
+        result.push(newAppt);
+      }
+    });
+    console.log(result);
+    return result;
+  }
 
   verify(appointment) {
     if (confirm('Are you sure you want to cancel?')) {
