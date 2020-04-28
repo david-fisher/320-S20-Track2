2be0918b91df9230a57eaf6898d1f3479f595673
Caleb Coffin <coffincw@gmail.com>
2020-04-28 17:43:07 -0400

    setup user_id and user_type cookies

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -13,7 +13,17 @@ import {AuthService} from '../auth/auth.service';
 export class NavComponent implements OnInit {
   @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
 
-  constructor(private cookieService: CookieService, private auth: AuthService) { }
+  constructor(private cookieService: CookieService, private auth: AuthService) {}
+
+  isStudent() {
+    console.log(this.cookieService.get('user_type'));
+    return this.cookieService.get('user_type') === 'student';
+  }
+
+  isSupporter() {
+    console.log(this.cookieService.get('user_type'));
+    return this.cookieService.get('user_type') === 'supporter';
+  }
 
   ngOnInit(): void {
   }
@@ -25,6 +35,7 @@ export class NavComponent implements OnInit {
     console.log('logged out');
     this.cookieService.delete('logged-in');
     this.cookieService.delete('user_id');
+    this.cookieService.delete('user_type');
     console.log('logged out');
   }
 
