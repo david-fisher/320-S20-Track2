84f13da46d849280ce4bed9a88d501dbe16bc14a
Bruce Waters <57501469+watersbruce@users.noreply.github.com>
2020-04-29 18:26:19 -0400

    Update supporter-appointments.component.ts

--- client/src/app/supporter/supporter-appointments/supporter-appointments.component.ts
+++ client/src/app/supporter/supporter-appointments/supporter-appointments.component.ts
@@ -5,6 +5,7 @@ import {TEST_APPOINTMENTS} from "./appointments";
 import { Router } from '@angular/router';
 import {InterestTags} from "../../admin/admin-tags/interest-tag";
 import {HttpClient, HttpResponse} from '@angular/common/http';
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
   selector: 'app-supporter-appointments',
@@ -17,9 +18,10 @@ export class SupporterAppointmentsComponent implements OnInit {
   tempFeedback;
   userID;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private cookieService: CookieService) {
     this.tempAppointments = this.appointments;
     this.tempFeedback = this.feedback(2);
+    this.userID = this.cookieService.get('user_id');
   }
 
   // get appointments(): Array<Appointment> {
@@ -32,8 +34,9 @@ export class SupporterAppointmentsComponent implements OnInit {
 
   get appointments(): Array<SupporterAppointment> {
     const result = [];
-    this.http.get('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/appointments/1', {}).subscribe(res => {
+    this.http.get('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/appointments/' + this.userID, {}).subscribe(res => {
       console.log(Object.values(res));
+      console.log("user ID:" + this.userID);
       for (const appt of Object.values(res)) {
         const newAppt : SupporterAppointment = {date: new Date(appt[2].split("-")[0], appt[2].split("-")[1], appt[2].split("-")[2], appt[3].split(":")[0], appt[3].split(":")[1], appt[3].split(":")[2], 0), type: appt[9], student: appt[7] + " " + appt[8], location: appt[10], duration: appt[4], appt_id: appt[0], cancelled: appt[6]};
         result.push(newAppt);
@@ -49,6 +52,26 @@ export class SupporterAppointmentsComponent implements OnInit {
     console.log(result);
     return result;
   }
+
+  // feedback(appt_id): SupporterFeedback {
+  //   //const result = {} as SupporterFeedback;
+  //   const result : SupporterFeedback = {questions: ["q1", "q2"], answers: ["a1", "a2"], rating: 0, recommend: true};
+  //   const qlist  = [];
+  //   const alist = [];
+  //   this.http.get('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/rate/'+appt_id, {}).subscribe(res => {
+  //     console.log(Object.values(res));
+  //     for (const appt of Object.values(res)) {
+  //       for(const qapair of appt[3]) {
+  //         qlist.push(qapair[0]);
+  //         alist.push(qapair[1]);
+  //       }
+  //       const result : SupporterFeedback = {questions: qlist, answers: alist, rating: appt[1], recommend: appt[2]};
+  //     }
+  //   });
+  //
+  //   console.log(result);
+  //   return result;
+  // }
 
   verify(appointment) {
     if (confirm('Are you sure you want to cancel?')) {
