14130ba92c01723ff94d2c4d4ded75ea6013f1d4
Bruce Waters <57501469+watersbruce@users.noreply.github.com>
2020-04-29 18:54:34 -0400

    Merge branch 'Front-End' of https://github.com/david-fisher/320-S20-Track2 into Front-End

--- client/src/app/supporter/supporter-appointments/supporter-appointments.component.ts
+++ client/src/app/supporter/supporter-appointments/supporter-appointments.component.ts
@@ -16,17 +16,11 @@ export class SupporterAppointmentsComponent implements OnInit {
 
   tempAppointments;
   tempFeedback;
-  userID;
 
   constructor(private http: HttpClient, private cookieService: CookieService) {
     this.tempAppointments = this.appointments;
     this.tempFeedback = this.feedback(2);
-    this.userID = this.cookieService.get('user_id');
   }
-
-  // get appointments(): Array<Appointment> {
-  //   return TEST_APPOINTMENTS;
-  // }
 
   get date(): object {
     return new Date();
@@ -34,20 +28,30 @@ export class SupporterAppointmentsComponent implements OnInit {
 
   get appointments(): Array<SupporterAppointment> {
     const result = [];
-    this.http.get('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/appointments/' + this.userID, {}).subscribe(res => {
+    this.http.get('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/appointments/' + this.cookieService.get('user_id'), {observe: 'response'}).subscribe(res => {
       console.log(Object.values(res));
-      console.log("user ID:" + this.userID);
-      for (const appt of Object.values(res)) {
-        const newAppt : SupporterAppointment = {date: new Date(appt[2].split("-")[0], appt[2].split("-")[1], appt[2].split("-")[2], appt[3].split(":")[0], appt[3].split(":")[1], appt[3].split(":")[2], 0), type: appt[9], student: appt[7] + " " + appt[8], location: appt[10], duration: appt[4], appt_id: appt[0], cancelled: appt[6]};
-        result.push(newAppt);
+      console.log("user ID:" + this.cookieService.get('user_id'));
+      if(res.status != 404) {
+        for (const appt of Object.values(res)) {
+          const newAppt: SupporterAppointment = {
+            date: new Date(appt[2].split("-")[0], appt[2].split("-")[1], appt[2].split("-")[2], appt[3].split(":")[0], appt[3].split(":")[1], appt[3].split(":")[2], 0),
+            type: appt[9],
+            student: appt[7] + " " + appt[8],
+            location: appt[10],
+            duration: appt[4],
+            appt_id: appt[0],
+            cancelled: appt[6]
+          };
+          // if(!newAppt.cancelled) {
+          //   result.push(newAppt);
+          // }
+        }
       }
     });
-    console.log(result);
     return result;
   }
 
   feedback(appt_id): SupporterFeedback {
-    //const result = {} as SupporterFeedback;
     const result : SupporterFeedback = {questions: ["q1", "q2"], answers: ["a1", "a2"], rating: appt_id, recommend: true};
     console.log(result);
     return result;
