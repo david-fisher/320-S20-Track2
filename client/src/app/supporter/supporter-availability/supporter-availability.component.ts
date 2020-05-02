ee09900edc6241267b7ab5bd7cd7a103d83e5fad
Jigar Patel <j.p.p408@gmail.com>
2020-05-01 23:52:25 -0400

    Reworked the supporter availability.

--- client/src/app/supporter/supporter-availability/supporter-availability.component.ts
+++ client/src/app/supporter/supporter-availability/supporter-availability.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {account} from '../../login/account';
 import {HttpClient} from '@angular/common/http';
+import {SupporterAppointment} from '../supporter-appointments/appointments';
 @Component({
   selector: 'app-supporter-availability',
   templateUrl: './supporter-availability.component.html',
@@ -8,84 +9,136 @@ import {HttpClient} from '@angular/common/http';
 })
 
 export class SupporterAvailabilityComponent implements OnInit {
-  sunday: string;
-  monday: string;
-  tuesday: string;
-  wednesday: string;
-  thursday: string;
-  friday: string;
-  saturday: string;
   body;
-
+  list: Array<string>;
+  field: string;
   date: string;
   start: string;
   end: string;
+  start24: string;
+  end24: string;
   startAMPM: string;
   endAMPM: string;
-  start24: number;
-  end24: number;
+  dates: Array<any>;
   constructor(private http: HttpClient) {
-    let sundayList = [], mondayList = [],tuesdayList = [], wednesdayList = [],thursdayList = [], fridayList = [], saturdayList = [];
-    //add all times from get request
-    sundayList.push('Unavailable');
-    mondayList.push('Unavailable');
-    tuesdayList.push('Unavailable');
-    wednesdayList.push('Unavailable');
-    thursdayList.push('Unavailable');
-    fridayList.push('Unavailable');
-    saturdayList.push('Unavailable');
-    this.sunday = sundayList.toString();
-    this.monday = sundayList.toString();
-    this.tuesday = sundayList.toString();
-    this.wednesday = sundayList.toString();
-    this.thursday = sundayList.toString();
-    this.friday = sundayList.toString();
-    this.saturday = sundayList.toString();
-
+    this.dates = [];
+    this.list = [];
+    this.field = "";
     this.body = {};
-    this.body.user_id = account.user_id;
+    this.body.user_id = account.user_id = "1";
+    this.updateAvailibility();
+  }
+  updateAvailibility(): void
+  {
+    this.dates = [];
+    let i = 0;
+    this.http.get('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/account/' + account.user_id,{}).subscribe(res => {
+      console.log(res);
+      for (const date of Object.values(res)) {
+        if(i > 1) {
+          console.log(date);
+          let dateObj = {year:date[3].split("-")[0],month: date[3].split("-")[1], date:date[3].split("-")[2], start_hour: date[1].split(":")[0], start_min:date[1].split(":")[1], end_hour:date[2].split(":")[0],end_min:date[2].split(":")[1]};
+          let a = "AM";
+          if(dateObj.start_hour >= 12) {
+            a = "PM";
+            if (dateObj.start_hour != 12) {
+              dateObj.start_hour -= 12;
+            }
+          }
+          else if(dateObj.start_hour === 0)
+          {
+            dateObj.start_hour = 12;
+          }
+          let b = "AM";
+          if(dateObj.end_hour >= 12)
+          {
+            b = "PM";
+            if(dateObj.end_hour != 12) {
+              dateObj.end_hour -= 12;
+            }
+          }
+          else if(dateObj.end_hour === 0)
+          {
+            dateObj.end_hour = 12;
+          }
+          let s = dateObj.month + "/" + dateObj.date +"/"+dateObj.year+ ": "+dateObj.start_hour + ":" + dateObj.start_min + " " + a + " - " + dateObj.end_hour + ":" + dateObj.end_min + " "+b;
+          console.log(s);
+          this.dates.push({str:s});
+        }
+        i++;
+      }
+      console.log(this.field);
+    });
   }
   add(): void {
     //add new avail
     if(this.timeValid()) {
-      this.body.availability_add = [{start_time: this.start24 * 60, end_time: this.end24 * 60, appt_date: this.date}];
+      this.body = {user_id: this.body.user_id, availability_add:[{start_time: this.start24, end_time: this.end24, appt_date: this.date}]};
       console.log(this.body);
-      this.http.patch('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/account/' + this.body.user_id, this.body).subscribe(res => {
+      this.http.patch('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/account/' + this.body.user_id,this.body).subscribe(res => {
         console.log(res);
       });
       this.body = {user_id: account.user_id};
+      this.updateAvailibility();
     }
   }
   remove(): void {
     //remove avail
     if(this.timeValid()) {
-      console.log(this.start24);
-      this.body.availability_delete = [{start_time: this.start24 * 60, end_time: this.end24 * 60, appt_date: this.date}];
+      this.body = {user_id: this.body.user_id, availability_delete:[{start_time: this.start24, end_time: this.end24, appt_date: this.date}]};
       console.log(this.body);
-      this.http.patch('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/account/' + this.body.user_id, this.body).subscribe(res => {
+      this.http.patch('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/account/' + this.body.user_id,this.body).subscribe(res => {
         console.log(res);
       });
       this.body = {user_id: account.user_id};
+      this.updateAvailibility();
     }
   }
   private timeValid(): boolean {
     if (this.date !== undefined && this.start !== undefined && this.end !== undefined && this.startAMPM !== undefined && this.endAMPM !== undefined) {
-      if (!this.start.includes(".") && !this.end.includes(".")&&!isNaN(Number(this.start)) && !isNaN(Number(this.end))) {
-        let s = Number(this.start);
-        let e = Number(this.end);
-        if(!(s > 0) && (s <= 12) && (e > 0) && (e <= 12))
+      if (!this.start.includes(".") && !this.end.includes(".")&&this.start.includes(":")&&this.end.includes(":")) {
+        let startarr = this.start.split(":");
+        let endarr = this.end.split(":");
+        let sh = Number(startarr[0]);
+        let sm = Number(startarr[1]);
+        let eh = Number(endarr[0]);
+        let em = Number(endarr[1]);
+        if(isNaN(sh) || isNaN(sm) || isNaN(eh) || isNaN(em))
         {
           return false;
         }
+        if(!((sh > 0) && (sh <= 12) && (eh > 0) && (eh <= 12)) && !((sm > 0) && (sm <= 59) && (em >= 0) && (em <= 59)))
+        {
+          return false;
+        }
+
         if (this.startAMPM === 'PM') {
-          s += 12;
+          if(sh !== 12) {
+            sh += 12;
+          }
+        }
+        else
+        {
+          if(sh === 12)
+          {
+            sh = 0;
+          }
         }
         if (this.endAMPM === 'PM') {
-           e += 12;
+          if(eh !== 12) {
+            eh += 12;
+          }
         }
-        this.start24 = s;
-        this.end24 = e;
-        return (e > s);
+        else
+        {
+          if(eh === 12)
+          {
+            eh = 0;
+          }
+        }
+        this.start24 = sh.toString() + ":" + sm.toString()+":00";
+        this.end24 = eh.toString() + ":" + em.toString()+":00";
+        return ((eh*60)+em) > ((sh*60)+sm);
       }
     }
     return false;
