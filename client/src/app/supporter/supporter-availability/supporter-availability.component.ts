808ee041ddcb9a106c3331ab19cda7bbc4cb2641
Jigar Patel <j.p.p408@gmail.com>
2020-05-02 03:41:21 -0400

    Updated supporter availability code

--- client/src/app/supporter/supporter-availability/supporter-availability.component.ts
+++ client/src/app/supporter/supporter-availability/supporter-availability.component.ts
@@ -9,11 +9,15 @@ import {CookieService} from "ngx-cookie-service";
 })
 
 export class SupporterAvailabilityComponent implements OnInit {
-  body;
-  list: Array<string>;
   date: string;
+  month:string;
+  year:string;
   start: string;
   end: string;
+  start_hour:string;
+  start_min:string;
+  end_hour:string;
+  end_min:string;
   start24: string;
   end24: string;
   startAMPM: string;
@@ -21,7 +25,6 @@ export class SupporterAvailabilityComponent implements OnInit {
   dates: Array<any>;
   constructor(private http: HttpClient,private cookieService: CookieService) {
     this.dates = [];
-    this.list = [];
     this.getAvailibility();
   }
   getAvailibility(): void
@@ -32,7 +35,7 @@ export class SupporterAvailabilityComponent implements OnInit {
       for (const date of Object.values(res)) {
         if(i > 1) {
           console.log(date);
-          let dateObj = {year:date[3].split("-")[0],month: date[3].split("-")[1], date:date[3].split("-")[2], start_hour: date[1].split(":")[0], start_min:date[1].split(":")[1], end_hour:date[2].split(":")[0],end_min:date[2].split(":")[1]};
+          let dateObj = {year:date[3].split("-")[0],month: date[3].split("-")[1], date:date[3].split("-")[2].split(":")[0], start_hour: date[1].split(":")[0], start_min:date[1].split(":")[1], end_hour:date[2].split(":")[0],end_min:date[2].split(":")[1]};
           let a = "AM";
           if(dateObj.start_hour >= 12) {
             a = "PM";
@@ -56,14 +59,6 @@ export class SupporterAvailabilityComponent implements OnInit {
           {
             dateObj.end_hour = 12;
           }
-          if(dateObj.start_min == 0)
-          {
-            dateObj.start_min = "00";
-          }
-          if(dateObj.end_min == 0)
-          {
-            dateObj.end_min = "00";
-          }
           let s = dateObj.month + "/" + dateObj.date +"/"+dateObj.year+ ": "+dateObj.start_hour + ":" + dateObj.start_min + " " + a + " - " + dateObj.end_hour + ":" + dateObj.end_min + " "+b;
           console.log(s);
           this.dates.push({str:s, start_time: date[1], end_time: date[2], appt_date: date[3]});
@@ -73,41 +68,17 @@ export class SupporterAvailabilityComponent implements OnInit {
     });
   }
   add(): void {
-    //add new avail
     if(this.timeValid()) {
-      const body = {user_id: this.cookieService.get('user_id'), availability_add:[{start_time: this.start24, end_time: this.end24, appt_date: this.date}]};
+      const body = {user_id: this.cookieService.get('user_id'), availability_add:[{start_time: this.start24, end_time: this.end24, appt_date: this.year + "-"+this.month + "-"+this.date}]};
       console.log(body);
       this.http.patch('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/account/' + this.cookieService.get('user_id'),body).subscribe(res => {
         console.log(res);
       });
-      this.body = {user_id: this.cookieService.get('user_id')};
-      let dateObj = {year:this.date.split("-")[0],month: this.date.split("-")[1], date:this.date.split("-")[2], start_hour: Number(this.start.split(":")[0]), start_min:Number(this.start.split(":")[1]), end_hour:Number(this.end.split(":")[0]),end_min:Number(this.end.split(":")[1])};
-      let a = "AM";
-      if(dateObj.start_hour >= 12) {
-        a = "PM";
-        if (dateObj.start_hour != 12) {
-          dateObj.start_hour -= 12;
-        }
-      }
-      else if(dateObj.start_hour == 0)
-      {
-        dateObj.start_hour = 12;
-      }
-      let b = "AM";
-      if(dateObj.end_hour >= 12)
-      {
-        b = "PM";
-        if(dateObj.end_hour != 12) {
-          dateObj.end_hour -= 12;
-        }
-      }
-      else if(dateObj.end_hour == 0)
-      {
-        dateObj.end_hour = 12;
-      }
-      let s = dateObj.month + "/" + dateObj.date +"/"+dateObj.year+ ": "+dateObj.start_hour + ":" + dateObj.start_min + " " + a + " - " + dateObj.end_hour + ":" + dateObj.end_min + " "+b;
+      let dateObj = {year:this.year,month: this.month, date:this.date, start_hour: this.start_hour, start_min:this.start_min, end_hour:this.end_hour,end_min:this.end_min};
+
+      let s = dateObj.month + "/" + dateObj.date +"/"+dateObj.year+ ": "+dateObj.start_hour + ":" + dateObj.start_min + " " + this.startAMPM + " - " + dateObj.end_hour + ":" + dateObj.end_min + " "+this.endAMPM;
       console.log(s);
-      this.dates.push({str:s,start_time: this.start24, end_time: this.end24, appt_date: this.date});
+      this.dates.push({str:s,start_time: this.start24, end_time: this.end24, appt_date: this.year + "-"+this.month + "-"+this.date});
     }
   }
   remove(date: any): void {
@@ -116,7 +87,6 @@ export class SupporterAvailabilityComponent implements OnInit {
       this.http.patch('https://lcqfxob7mj.execute-api.us-east-2.amazonaws.com/dev/account/' + this.cookieService.get('user_id'),body).subscribe(res => {
         console.log(res);
       });
-      this.body = {user_id: this.cookieService.get('user_id')};
     for (const x in this.dates) {
       if(this.dates[x].str === date.str)
       {
@@ -125,7 +95,7 @@ export class SupporterAvailabilityComponent implements OnInit {
     }
   }
   private timeValid(): boolean {
-    if (this.date !== undefined && this.start !== undefined && this.end !== undefined && this.startAMPM !== undefined && this.endAMPM !== undefined) {
+    if (this.date !== undefined && this.month !== undefined && this.year !== undefined && this.start !== undefined && this.end !== undefined && this.startAMPM !== undefined && this.endAMPM !== undefined) {
       if (!this.start.includes(".") && !this.end.includes(".")&&this.start.includes(":")&&this.end.includes(":")) {
         let startarr = this.start.split(":");
         let endarr = this.end.split(":");
@@ -133,6 +103,18 @@ export class SupporterAvailabilityComponent implements OnInit {
         let sm = Number(startarr[1]);
         let eh = Number(endarr[0]);
         let em = Number(endarr[1]);
+        this.start_hour = sh.toString();
+        this.start_min = sm.toString();
+        this.end_hour = eh.toString();
+        this.end_min = em.toString();
+        if(sm <= 9)
+        {
+          this.start_min = "0" + sm;
+        }
+        if(em <= 9)
+        {
+          this.end_min = "0" + em;
+        }
         if(isNaN(sh) || isNaN(sm) || isNaN(eh) || isNaN(em))
         {
           return false;
