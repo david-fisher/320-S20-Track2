04302a16a17d01a033f8665777527e16683c34f7
knsanchez0001 <35647594+knsanchez0001@users.noreply.github.com>
2020-05-04 06:06:14 -0400

    woof
    Added capability of fetching appointments for students.
    Added a boolean value for whether an appointment has been rating
    Removed 404 status error; if there are no appointments, simply returns an empty array
    Added a whole lot of messiness to the code, possibly some unseen bugs
    
    https://www.youtube.com/watch?v=83m261lAlrs

--- appointments/appointments-get.py
+++ appointments/appointments-get.py
@@ -19,23 +19,92 @@ def get_appointments(event, context):
         return {
             'statusCode' : 400,
             'headers' : response_headers,
-            'body' : json.dumps({'message' : 'Invalid input'})
+            'body' : json.dumps({'message' : 'Invalid user id input'})
         }
    
-    query = "SELECT A.*, U.first_name, U.last_name, T.appointment_name, S.location FROM appointments A NATURAL JOIN user U NATURAL JOIN appointment_type T NATURAL JOIN supporter S WHERE A.supporter_id = :u_id AND U.user_id_ = :u_id;"
+    # Checks if user_id_ is a student id
+    query = "SELECT user_id_ FROM student WHERE user_id_ = :u_id"
+    sql_params = [
+        {'name' : 'u_id', 'value': {'longValue': u_id}}
+    ]
+    student_check = execute_statement(query, sql_params)
+    
+    # Checking if the given id does not exist in student then run query for supporter else run query for student
+    if(student_check['records'] == []): # Gather for supporters
+        query = (
+            "SELECT "
+                "A.*, U.first_name, U.last_name, T.appointment_name, S.location "
+                ", SF.rating "
+            "FROM appointments A "
+                "LEFT JOIN user U ON U.user_id_ = A.supporter_id "
+                "LEFT JOIN appointment_type T ON T.type_id = A.type_id " 
+                "LEFT JOIN supporter S ON S.user_id_ = U.user_id_ " 
+                "LEFT JOIN student_feedback SF ON SF.appointment_id = A.appointment_id "
+                "LEFT JOIN student_responses SR ON SR.appointment_id = A.appointment_id "
+            "WHERE A.supporter_id = :u_id GROUP BY A.appointment_id ; "
+        )
+    else: # Gather for students
+        query = (
+            "SELECT "
+                "A.*, U.first_name, U.last_name, T.appointment_name, SU.location "
+                ", SF.rating "
+            "FROM appointments A "
+                "LEFT JOIN student_feedback SF ON SF.appointment_id = A.appointment_id "
+                "LEFT JOIN student S ON SF.student_id = S.user_id_ "
+                "LEFT JOIN user U ON U.user_id_ = A.supporter_id "
+                "LEFT JOIN appointment_type T ON T.type_id = A.type_id "
+                "LEFT JOIN supporter SU ON SU.user_id_ = U.user_id_ "
+                "LEFT JOIN student_responses SR ON SR.appointment_id = A.appointment_id "
+            "WHERE S.user_id_ = :u_id   GROUP BY A.appointment_id; "
+        )
+    
     sql_params = [{'name': 'u_id', 'value':{'longValue': u_id}}]
     query_result = execute_statement(query, sql_params)
+    response = extract_records(query_result)
     
-    if query_result['records'] == []:
-        return {
-         'statusCode' : 404,
-         'headers' : response_headers,
-         'body' : json.dumps({'message' : 'No appointments found'})
-        }
-   
+    query = "SELECT user_id_ FROM student WHERE user_id_ = :u_id"
+    sql_params = [
+        {'name' : 'u_id', 'value': {'longValue': u_id}}
+    ]
+    student_check = execute_statement(query, sql_params)
+    
+    # Checking again but for non-null ratings
+    if(student_check['records'] == []): # Gather for supporters
+        query2 = (
+            "SELECT "
+                "A.appointment_id, SF.rating "
+            "FROM appointments A " 
+                "LEFT JOIN student_feedback SF ON SF.appointment_id = A.appointment_id "
+                "LEFT JOIN student_responses SR ON SR.appointment_id = A.appointment_id "
+            "WHERE A.supporter_id = :u_id AND SF.rating IS NOT NULL GROUP BY A.appointment_id; "
+        )
+    else: # Gather for students
+        query2 = (
+            "SELECT "
+                "A.appointment_id, SF.rating "
+            "FROM appointments A "
+                "LEFT JOIN student_feedback SF ON SF.appointment_id = A.appointment_id "
+                "LEFT JOIN student S ON SF.student_id = S.user_id_ "
+                "LEFT JOIN user U ON U.user_id_ = A.supporter_id "
+            "WHERE S.user_id_ = :u_id AND SF.rating IS NOT NULL GROUP BY A.appointment_id; "
+        )
+           
+    query_result = execute_statement(query2, sql_params)
+    
+    response2 = extract_records(query_result)
+    
+    for entry in response:
+        if response2 == []:
+            entry[11] = False
+        elif entry[0] == response2[0][0] and entry[11]:
+            entry[11] = True
+            response2.pop(0)
+        else:
+            entry[11] = False
+        
+            
     return {
         'statusCode' : 200,
         'headers' : response_headers,
-        'body' : json.dumps(extract_records(query_result))
+        'body' : json.dumps(response)
     }
-
